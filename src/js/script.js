86a3397f0f0308ec25e9b16b5bd3bdf3956da2c1
Konstantin <konstantinokhorzin@gmail.com>
2022-06-20 16:00:46 +0300

    Post request

--- src/js/script.js
+++ src/js/script.js
@@ -265,7 +265,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // Modal----------------------------------------------------------------------------------------------
   const modalTrigger = document.querySelector('[data-modal-consultation]');
   const modal = document.querySelector('#consultation');
-  const modalCloseBtn = document.querySelector('.modal__close');
   const overlay = document.querySelector('.overlay');
 
   function openModal() {
@@ -282,10 +281,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.style.overflow = '';
   }
 
-  modalCloseBtn.addEventListener('click', closeModal);
-
   overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
+    if (e.target === overlay || e.target.classList.contains('modal__close')) {
       closeModal();
     }
   });
@@ -389,6 +386,73 @@ document.addEventListener('DOMContentLoaded', () => {
     'card-courses'
   ).render();
 
+  // Form---------------------------------------------------------------------------------------------
+    const formConsultation = document.querySelector('#form-consultation');
+
+    const message = {
+      loading: '../icons/spinner.svg',
+      success: 'Thanks! We will contact you shortly',
+      failure: 'Sorry! Server invalide'
+    };
+
+    const postData = async (url, data) => {
+      const result = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: data
+      });
+
+      if (!result.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${result.staus}`);
+      }
+
+      return await result.json();
+    };
+
+    bindPostData(formConsultation);
+
+    function bindPostData(form) {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const statusMessage = document.createElement('img');
+        statusMessage.src = message.loading;
+        statusMessage.classList.add('modal__spinner');
+        statusMessage.textContent = message.loading;
+        form.append(statusMessage);
+
+        const formData = new FormData(form);
+
+        const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+        postData('http://localhost:3000/consultation', json)
+        .then(data => {
+            showThanksModal(message.success);
+        }).catch(() => {
+            showThanksModal(message.failure);
+        }).finally (() => {
+            form.reset();
+            statusMessage.remove();
+        });
+      });
+    }
+
+    function showThanksModal(message) {
+      modal.classList.remove('modal_active');
+      const thanksModal = document.createElement('div');
+      thanksModal.classList.add('modal', 'modal_active');
+      thanksModal.innerHTML = `
+      <button class="modal__close"></button>
+      <div class="modal__title">${message}</div>
+      `;
+      overlay.append(thanksModal);
+      setTimeout(() => {
+        thanksModal.remove();
+        closeModal();
+      }, 2000);
+    }
 });
 
 
