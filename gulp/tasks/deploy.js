766e5463338d302866a60cb00e83f882e4f734ff
Kostiantyn Okhorzin <kostiantynokhorzin@gmail.com>
2023-10-02 16:47:06 +0300

    feat: Update option cacheDir for gh-pages in deploy task

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -4,7 +4,11 @@ import ghPages from 'gulp-gh-pages';
 import path from '../config/path.js';
 
 const deploy = () => {
-  return gulp.src(path.deploy.dest).pipe(ghPages());
+  return gulp.src(path.deploy.dest).pipe(
+    ghPages({
+      cacheDir: 'publish',
+    })
+  );
 };
 
 export default deploy;
